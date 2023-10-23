d9734df2128da4c5a75f0cd908ab40bff4155bf1
keaganwong <2837260W@student.gla.ac.uk>
2023-10-23 14:51:09 +0800

    Added cookies
    Added cookies to rmbr user lang preferences

--- cloud/client/src/App.js
+++ cloud/client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
+import { getCookie, setCookie } from "./cookieUtils"; // Import your cookie utility functions.
 
 const socket = io.connect("http://localhost:3001");
 
@@ -9,9 +10,27 @@ function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [showChat, setShowChat] = useState(false);
+  const [yourLanguage, setYourLanguage] = useState(getCookie("userLanguage") || ""); // Initialize with the value from the cookie, if available.
+  const [learnLanguage, setLearnLanguage] = useState(getCookie("newLang") || ""); // Initialize with the value from the cookie, if available.
+
+  const languageOptions = [
+    { value: "en", label: "English" },
+    { value: "es", label: "Español" },
+    { value: "kr", label: "Korean" },
+    { value: "jp", label: "Japanese" },
+    // Add more language options as needed
+  ];
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
+      // Store the language preferences in cookies when joining the room.
+      if (yourLanguage !== "") {
+        setCookie("userLanguage", yourLanguage, 365); // You can adjust the expiration time as needed.
+      }
+      if (learnLanguage !== "") {
+        setCookie("newLang", learnLanguage, 365); // You can adjust the expiration time as needed.
+      }
+
       socket.emit("join_room", room);
       setShowChat(true);
     }
@@ -36,6 +55,36 @@ function App() {
               setRoom(event.target.value);
             }}
           />
+          <div>
+            <select
+              value={yourLanguage}
+              onChange={(event) => {
+                setYourLanguage(event.target.value);
+              }}
+            >
+              <option value="">Select Your Language</option>
+              {languageOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <select
+              value={learnLanguage}
+              onChange={(event) => {
+                setLearnLanguage(event.target.value);
+              }}
+            >
+              <option value="">Select Language to Learn</option>
+              {languageOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
           <button onClick={joinRoom}>Join A Room</button>
         </div>
       ) : (
---
+++ cloud/client/src/cookieUtils.js
@@ -0,0 +1,13 @@
+// cookieUtils.js
+export function getCookie(name) {
+    const cookieValue = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return cookieValue ? cookieValue[2] : null;
+  }
+  
+  export function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + "; " + expires + "; path=/";
+  }
+  
