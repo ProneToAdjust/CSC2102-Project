7fa37b7d69bfd2a38a449b2784ee5b1486157be8
gabriellau- <40619564+gabriel-lau@users.noreply.github.com>
2023-10-30 01:43:50 +0800

    Error checking and updated dockerfiles + some kub

--- cloud/backend/containerManager.py
+++ cloud/backend/containerManager.py
@@ -28,10 +28,11 @@ class Container:
 # Param: None
 # Return: containerID - the ID of the container created
 def createContainer() -> str:
+    port = _getAvailablePort()
     # Create a container
-    container = client.containers.run("server", "sleep 100", detach=True, ports={'3001/tcp': _getAvailablePort()})
+    container = client.containers.run("server:latest", detach=True, ports={'3001/tcp': port})
     # Add the container to the dictionary
-    containers[container.id] = Container(container.ports)
+    containers[container.id] = Container(port)
     # Return the container ID
     return container.id
 
@@ -40,6 +41,7 @@ def createContainer() -> str:
 # Return: None
 def deleteContainer(containerID: str) -> None:
     # Delete the container
+    client.containers.get(containerID).stop()
     client.containers.get(containerID).remove()
     # Remove the container from the dictionary
     del containers[containerID]
@@ -49,27 +51,32 @@ def deleteContainer(containerID: str) -> None:
 #        userID - the ID of the user to set
 # Return: True if successful, False if full
 def setContainerUserID(containerID: str, userID: int) -> bool:
-    if (containers[containerID].userIDs[0] == None):
-        containers[containerID].userIDs[0] = userID
-        return True
-    elif (containers[containerID].userIDs[1] == None):
-        containers[containerID].userIDs[1] = userID
-        return True
-    else:
-        return False
+    if containerID in containers:
+        if (containers[containerID].userIDs[0] == None or containers[containerID].userIDs[0] == userID):
+            containers[containerID].userIDs[0] = userID
+            return True
+        elif (containers[containerID].userIDs[1] == None):
+            containers[containerID].userIDs[1] = userID
+            return True
+    return False
 
 # Gets the userIDs associated with a container
 # Param: containerID - the ID of the container to get the userIDs of
 # Return: user1ID - the ID of the first user
 #         user2ID - the ID of the second user
 def getContainerUserIDs(containerID: str):
-    return containers[containerID].userIDs[0], containers[containerID].userIDs[1]
+    if containerID in containers:
+        return containers[containerID].userIDs[0], containers[containerID].userIDs[1]
+    return None, None
 
 # Gets the port associated with a container
 # Param: containerID - the ID of the container to get the port of
 # Return: port - the port of the container
 def getContainerPort(containerID: str) -> int:
-    return containers[containerID].port
+    if containerID in containers:
+        return containers[containerID].port
+    else:
+        return None
 
 def _getAvailablePort() -> int:
     for port in range(49152, 65535):
