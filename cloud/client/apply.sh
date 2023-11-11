69824669d290d8265e23c3e8d55c0f49672ba89d
ProneToAdjust <41341852+ProneToAdjust@users.noreply.github.com>
2023-11-11 22:23:52 +0800

    Update metrics server and client deployment

--- cloud/client/apply.sh
+++ cloud/client/apply.sh
@@ -1,8 +1,9 @@
 docker build -t client .
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl delete -f https://raw.githubusercontent.com/pythianarora/total-practice/master/sample-kubernetes-code/metrics-server.yaml
 kubectl delete -f client-deployment.yaml
 kubectl delete -f client-service.yaml
 kubectl delete -f client-hpa.yaml
+kubectl create -f https://raw.githubusercontent.com/pythianarora/total-practice/master/sample-kubernetes-code/metrics-server.yaml
 kubectl apply -f client-deployment.yaml
 kubectl apply -f client-service.yaml
 kubectl apply -f client-hpa.yaml
